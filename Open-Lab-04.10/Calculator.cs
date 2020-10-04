ed1d8b0828ce32decc10ff53804b6bc13b36cd92
jakub kapitulcin <kapitulcinjakub1@gmail.com>
2020-10-04 09:51:28 +0200

    I make function to retutn avg value oj array

--- Open-Lab-04.10/Calculator.cs
+++ Open-Lab-04.10/Calculator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Open_Lab_04._10
 {
@@ -6,7 +7,7 @@ namespace Open_Lab_04._10
     {
         public float Average(int[] nums)
         {
-            throw new NotImplementedException();
+            return (float)nums.Sum() / nums.Length;
         }
     }
 }
